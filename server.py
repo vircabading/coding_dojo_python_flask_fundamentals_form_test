7128ceff499efb73582274f7d779b597a265d9c2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-28 20:23:40 -0700

    format final

--- server.py
+++ server.py
@@ -3,11 +3,11 @@
 # By: Virgilio D. Cabading Jr.  Created: October 28, 2021
 # /////////////////////////////////////////////////////////////////////
 
-from flask import Flask, render_template, request, redirect    # Import Flask to allow us to create our app
-app = Flask(__name__)                                           # Create a new instance of the Flask class called "app"
+from flask import Flask, render_template, request, redirect         # Import Flask to allow us to create our app
+app = Flask(__name__)                                               # Create a new instance of the Flask class called "app"
 
 # **** Default App Route **********************************************
-@app.route('/')                                         # The "@" decorator associates this route with the function immediately following
+@app.route('/')                                                     # The "@" decorator associates this route with the function immediately following
 def index():
     return render_template('index.html')
 
@@ -16,9 +16,9 @@ from flask import Flask, render_template, request, redirect # added request
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
+    print(request.form)
+    # Never render a template on a POST request.
+    # Instead we will redirect to our index route.
     return redirect('/')
 
 
